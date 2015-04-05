747b58f5bcdcba44d94f2c7c62ed963d91e3a9f4
plant <plant@canopus.uberspace.de>
2015-04-05 12:04:55 +0200

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  reality = models.TextField(max_length=2500)
+  story = models.TextField(max_length=2500)
+  pub_date = models.DateTimeField('date published')
