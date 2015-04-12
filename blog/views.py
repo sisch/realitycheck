b166d6f17c1ce20b590b7eaff268564e20b69f88
sisch <sisch105@hhu.de>
2015-04-12 19:25:34 +0200

    removed function for loading further posts (deprecated)

--- blog/views.py
+++ blog/views.py
@@ -16,16 +16,6 @@ def index(request):
     })
     return HttpResponse(template.render(context))
 
-def get_next_three_posts(request, id):
-    # -1 offset, because ids are 1-indexed in rendered HTML file
-    latest_id = max(int(id) - 1, 0)
-    oldest_id = max(int(id) - 3, 0)
-    print("%d :: %d"%(latest_id, oldest_id))
-    post_list = Post.objects.order_by('pub_date')[oldest_id:latest_id+1]
-    posts = reversed(post_list)
-    html = render_to_string('blog/template.html', {'post_list': posts,'home': False, })
-    return HttpResponse(html)
-
 def get_next(request, timestamp):
     dt = datetime.datetime.fromtimestamp(float(timestamp))
     post_list = Post.objects.all().filter(pub_date__lte=dt).order_by('-pub_date')[1:2]
