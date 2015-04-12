a08feb749425be36bccda448397e89d7fcb9a51a
sisch <sisch105@hhu.de>
2015-04-12 19:32:57 +0200

    Removed deprecated urls link as well

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url
 from blog import views
 
 urlpatterns = patterns('',
-    url(r'^getNextPost/(?P<id>\d+)/$', views.get_next_three_posts, name='getNextPost'),
     url(r'^api/posts/bytime/(?P<timestamp>\d+)/$', views.post_detail, name='postDetailByTime'),
     url(r'^api/posts/getNext/(?P<timestamp>\d+)/$', views.get_next, name='getNext'),
     url(r'^api/posts/(?P<id>\d+)/$', views.post_detail, name='postDetailByID'),
