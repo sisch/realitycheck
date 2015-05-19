47fc61fb64319ea07b2fff3abf14f96f44d95c68
sisch <sisch105@hhu.de>
2015-05-19 09:17:08 +0200

    Backend link shows new domain now

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,7 @@ class BlogAdmin(admin.ModelAdmin):
   list_display = ('title','pub_date', 'get_pub_date_unix')
   fields = (('title','pub_date'),('reality','story'))
   def get_pub_date_unix(self, instance):
-    return "<a href='http://realitycheck.plant.canopus.uberspace.de/api/posts/bytime/{}'>Link</a>".format(int(time.mktime(instance.pub_date.timetuple())))
+    return "<a href='http://realitycheck.pl/api/posts/bytime/{}'>Link</a>".format(int(time.mktime(instance.pub_date.timetuple())))
   get_pub_date_unix.allow_tags = True
   pass
 
