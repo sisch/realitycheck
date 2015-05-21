84fa9f69af35d4534e086565aa01145d635e1b21
sisch <sisch105@hhu.de>
2015-05-21 19:25:47 +0200

    Pah, standards

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.template.loader import render_to_string
 from django.http import HttpResponse
+from django.utils import feedgenerator
 from blog.models import Post
 import datetime
 
