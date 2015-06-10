bd7eb681a6337986ed6a428df35b9883ce6774fa
sisch <sisch105@hhu.de>
2015-06-10 21:30:53 +0200

    Adjusted urls for detail posts and limited other posts in detail view to 4

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,7 @@ class BlogAdmin(admin.ModelAdmin):
   list_display = ('title','pub_date', 'hidden', 'get_pub_date_unix')
   fields = (('title','pub_date','hidden'),('reality','story'))
   def get_pub_date_unix(self, instance):
-    return "<a href='http://realitycheck.pl/api/posts/bytime/{}'>Link</a>".format(int(time.mktime(instance.pub_date.timetuple())))
+    return "<a href='http://realitycheck.pl/post/{}/'>Link</a>".format(int(time.mktime(instance.pub_date.timetuple())))
   get_pub_date_unix.allow_tags = True
   pass
 
