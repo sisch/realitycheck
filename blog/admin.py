a1852357157572808529857facd619a553d6ac82
Simon Schliesky <simon.schliesky@hhu.de>
2015-12-21 19:01:57 +0100

    Timezones gnarf: Tweet time in admin interface diverged from detail pages

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,7 @@ class BlogAdmin(admin.ModelAdmin):
   list_display = ('title','pub_date', 'visible', 'get_twitter_post')
   fields = (('title','pub_date','visible'),('reality','reality_image'),('story','story_image'))
   def get_twitter_post(self, instance):
-    tweet = "Neuer Blogeintrag #RealityCheck:\n"+instance.title+"\n\nhttps://realitycheck.pl/post/{}/".format(int(time.mktime(instance.pub_date.timetuple())))+"\n\n({} / {})".format(instance.reality_wordcount, instance.story_wordcount)
+    tweet = "Neuer Blogeintrag #RealityCheck:\n"+instance.title+"\n\nhttps://realitycheck.pl/post/{}/".format(int(time.mktime(instance.pub_date.timetuple())+3600))+"\n\n({} / {})".format(instance.reality_wordcount, instance.story_wordcount)
     return "<a href='https://twitter.com/intent/tweet?text={}'>Tweet this</a>".format(urllib.quote(tweet.encode("utf-8")))
   get_twitter_post.allow_tags = True
   pass
