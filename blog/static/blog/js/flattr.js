5dc1e8bca3cbf3c599c6115bd7f4aac5ac311d13
PlantProgrammer <plant@canopus.uberspace.de>
2018-01-07 15:02:39 +0100

    remove Flattr

--- blog/static/blog/js/flattr.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var s = document.createElement('script');
-  var t = document.getElementsByTagName('script')[0];
-  s.type = 'text/javascript';
-  s.async = true;
-  s.src = '//api.flattr.com/js/0.6/load.js?'+
-          'mode=auto&uid=PlantProgrammer&language=de_DE&category=text';
-  t.parentNode.insertBefore(s,t);
-})();
